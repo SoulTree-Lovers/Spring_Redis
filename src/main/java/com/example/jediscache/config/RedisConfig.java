7e59e4544b7dd59a031bde9e20b967e3c8984278
kangseungmin <rkdtmdals999@naver.com>
2024-02-26 22:20:55 +0900

    [feat] RedisTemplate 구현

--- src/main/java/com/example/jediscache/config/RedisConfig.java
+++ src/main/java/com/example/jediscache/config/RedisConfig.java
@@ -1,10 +1,22 @@
 package com.example.jediscache.config;
 
+import com.example.jediscache.model.User;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.JedisPool;
 
@@ -13,6 +25,43 @@ import redis.clients.jedis.JedisPool;
         JmxAutoConfiguration.class
 })
 public class RedisConfig {
+
+    @Bean
+    RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        var objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // 속성을 모를 때 무효처리
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        var template = new RedisTemplate<String, User>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, User.class));
+
+        return template;
+    }
+
+    @Bean
+    RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        PolymorphicTypeValidator polymorphicTypeValidator = BasicPolymorphicTypeValidator
+            .builder()
+            .allowIfSubType(Object.class)
+            .build();
+
+        var objectMapper = new ObjectMapper()
+            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // 속성을 모를 때 무효처리
+            .registerModule(new JavaTimeModule())
+            .activateDefaultTyping(polymorphicTypeValidator, ObjectMapper.DefaultTyping.NON_FINAL)
+            .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        var template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+        return template;
+    }
 
     @Bean
     public JedisPool createJedisPool() {
