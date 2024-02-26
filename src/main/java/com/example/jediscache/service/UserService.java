dd65b2451ac5a4371545d8d2e1ff888ad9299719
kangseungmin <rkdtmdals999@naver.com>
2024-02-26 22:36:00 +0900

    [feat] RedisHashUser Service 구현

--- src/main/java/com/example/jediscache/service/UserService.java
+++ src/main/java/com/example/jediscache/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.jediscache.service;
 
+import com.example.jediscache.model.RedisHashUser;
 import com.example.jediscache.model.User;
+import com.example.jediscache.repository.RedisHashUserRepository;
 import com.example.jediscache.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,6 +15,7 @@ import java.time.Duration;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RedisHashUserRepository redisHashUserRepository;
     private final RedisTemplate<String, User> userRedisTemplate;
     private final RedisTemplate<String, Object> objectRedisTemplate;
 
@@ -29,5 +32,21 @@ public class UserService {
         User dbUser = userRepository.findById(id).orElseThrow();
         objectRedisTemplate.opsForValue().set(key, dbUser, Duration.ofSeconds(30));
         return dbUser;
+    }
+
+    public RedisHashUser getRedisHashUser(final Long id) {
+        // Redis에 값이 있으면 리턴, 없으면 DB에서 가져오기
+        var cachedUser = redisHashUserRepository.findById(id).orElseGet(() -> {
+            User dbUser = userRepository.findById(id).orElseThrow();
+            return redisHashUserRepository.save(RedisHashUser.builder()
+                    .id(dbUser.getId())
+                    .name(dbUser.getName())
+                    .email(dbUser.getEmail())
+                    .createdAt(dbUser.getCreatedAt())
+                    .updatedAt(dbUser.getUpdatedAt())
+                    .build());
+        });
+
+        return cachedUser;
     }
 }
