3b2313a75153deb723f16176284f7f3d820b7754
kangseungmin <rkdtmdals999@naver.com>
2024-02-26 22:21:14 +0900

    [feat] @EnableJpaAuditing 추가

--- src/main/java/com/example/jediscache/JediscacheApplication.java
+++ src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 @RequiredArgsConstructor
 public class JediscacheApplication implements ApplicationRunner {
