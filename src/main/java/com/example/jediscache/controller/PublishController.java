6693841aa09442d4634c824b28f79094a237e632
kangseungmin <rkdtmdals999@naver.com>
2024-02-29 11:57:38 +0900

    [feat] RedisTemplate을 이용한 API 구현

---
+++ src/main/java/com/example/jediscache/controller/PublishController.java
@@ -0,0 +1,18 @@
+package com.example.jediscache.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class PublishController {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("/users/unregister")
+    public void publishUserUnregister() {
+        redisTemplate.convertAndSend("users:unregister", "999");
+    }
+}
